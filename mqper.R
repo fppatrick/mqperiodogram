1adf47c909a3b64d499d3055045b637c83476f95
Patrick <patrick_patrocinio@hotmail.com>
2025-06-28 13:17:17 -0300

    Add files via upload

--- mqper.R
+++ mqper.R
@@ -3,38 +3,34 @@
 #' M-quantile regression.
 #' @param series univariate time series
 #' @param tau quantile level (0 < tau < 1)
-#' @return a numeric vector containing the robust estimates of the spectral density
+#' @return spec: Vector of robust level-crossings spectral estimates
+#' @return freq: Vector of corresponding frequencies
 #' @author Patrick Ferreira Patrocinio
-
+#' @import MASS
+library(MASS)
 mqper <- function(series, tau) {
   n <- length(series)
-  perior <- FFT <- NULL
   g <- n %/% 2
-  for (j in 1:g) {
+  per <- FFT <- rep(0,g)
+  for (j in 1:(g-1)) {
     X1 <- X2 <- NULL
     w <- 2 * pi * j / n
-    for (i in 1:n) {
-      X1[i] <- cos(w * i)
-      X2[i] <- sin(w * i)
-    }
-    if (j != (n / 2)) {
-      MX <- cbind(X1, X2)
-      fitrob <- mqlm(MX, series, maxit=100, q=tau, k=1.345)
-      FFT[j] <- sqrt(n / (8 * pi)) * complex(real = fitrob$coef[1], imaginary = -fitrob$coef[2])
-    }
-    else {
-      MX <- cbind(X1)
-      fitrob <- mqlm(MX, series, maxit=100, q=tau, k=1.345)
-      FFT[j] <- sqrt(n / (2 * pi)) * complex(real = fitrob$coef[1], imaginary = -0)
-    }
-    perior[j] <- Mod(FFT[j])^2
+    X1 <- cos(w * 1:n)
+    X2 <- sin(w * 1:n)
+    MX <- cbind(X1, X2)
+    fitrob <- mqlm(MX, series, maxit=100, q=tau, k=1.345)
+    FFT[j] <- sqrt(n / (8 * pi)) * complex(real = fitrob$coefficients[2], imaginary = -fitrob$coefficients[3])
+    
   }
-  if ((n %% 2) != 0) {
-    spec <- c(perior, rev(perior))
-  } else {
-    spec <- c(perior, rev(perior))
-  }
+  w <- 2 * pi * seq_len(g) / n
+  X1 <- cos(w * 1:n)
+  fitrob <- mqlm(MX, series, maxit=100, q=tau, k=1.345)
+  FFT[g] <- sqrt(n / (8 * pi)) * complex(real = fitrob$coefficients[2], imaginary = -0)
+  per <- Mod(FFT)^2
+  
+  perior <- c(per[-1], rev(per))
+  
   w <- 2 * pi * seq.int(g) / n
-  return(list(spec = spec, freq = w))
+  return(list(spec = perior, freq = w))
 }
 
